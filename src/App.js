76af2b8efc8243353196cc7946ba8270bc91d7fd
pixelmate <prince.zlx9211@gmail.com>
2021-05-09 01:54:59 +0530

    Working

--- src/App.js
+++ src/App.js
@@ -1,13 +1,154 @@
 /* eslint-disable no-lone-blocks */
-import React from 'react';
+import React, { Fragment } from 'react';
 import './App.css';
-
 class App extends React.Component {
-  // constructor(props) {
-  //   super(props);
+  constructor(props) {
+    super(props);
+    this.state = {
+      isWinner: false,
+      userClick: 0,
+      computerClick: 0,
+      isPlayerTurn: true,
+      tieGames: 0,
+      computerWin: 0,
+      playerWin: 0,
+      isClickable: true,
+    };
+  }
 
-  //   this.state = [(playerWin = 0), (tieGames = 0), (computerWin = 0)];
-  // }
+  handleClick = (index) => {
+    if (!this.state.isClickable) {
+      return;
+    }
+
+    if (this.refs['block' + index].className.length > 0) {
+      return;
+    }
+
+    if (this.state.isWinner) {
+      return;
+    }
+
+    this.refs['block' + index].className = 'x';
+    this.refs['block' + index].classList.add('x');
+    this.checkForWinner();
+    this.setState(
+      {
+        userClick: parseInt(this.state.userClick) + 1,
+        isClickable: false,
+      },
+      () => {
+        this.computerTurn(index);
+      }
+    );
+  };
+
+  resetGame = () => {
+    for (let i = 1; i <= 9; i++) {
+      this.refs['block' + i].classList = '';
+    }
+
+    this.setState({
+      userClick: 0,
+      computerClick: 0,
+      isWinner: false,
+      isClickable: true,
+    });
+  };
+
+  computerTurn = () => {
+    if (this.state.isWinner) {
+      return;
+    }
+
+    let notBlueOrRed = [];
+    let isAvailable = false;
+    for (let i = 1; i <= 9; i++) {
+      if (this.refs['block' + i].className.length === 0) {
+        notBlueOrRed.push(i);
+        isAvailable = true;
+      }
+    }
+
+    if (isAvailable) {
+      setTimeout(() => {
+        var randomItem =
+          notBlueOrRed[Math.floor(Math.random() * notBlueOrRed.length)];
+        this.refs['block' + randomItem].className = 'o';
+        this.refs['block' + randomItem].classList.add('o');
+        this.checkForWinner();
+        this.setState({
+          computerClick: parseInt(this.state.computerClick) + 1,
+          isClickable: true,
+        });
+      }, 500);
+    } else {
+      if (!this.state.isWinner) {
+        this.setState(
+          {
+            tieGames: parseInt(this.state.tieGames) + 1,
+          },
+          () => {
+            setTimeout(() => {
+              this.resetGame();
+            }, 2000);
+          }
+        );
+      }
+    }
+  };
+
+  checkForWinner = () => {
+    const combinationCollection = [
+      [1, 2, 3],
+      [4, 5, 6],
+      [7, 8, 9],
+      [1, 4, 7],
+      [2, 5, 8],
+      [3, 6, 9],
+      [1, 5, 9],
+      [3, 5, 7],
+    ];
+    combinationCollection.map((data) => {
+      if (
+        this.refs['block' + data[0]].className &&
+        this.refs['block' + data[1]].className &&
+        this.refs['block' + data[2]].className &&
+        this.refs['block' + data[0]].className.toLowerCase() ===
+          this.refs['block' + data[1]].className.toLowerCase() &&
+        this.refs['block' + data[1]].className.toLowerCase() ===
+          this.refs['block' + data[2]].className.toLowerCase() &&
+        this.refs['block' + data[2]].className.toLowerCase() ===
+          this.refs['block' + data[0]].className.toLowerCase()
+      ) {
+        {
+          this.setState(
+            {
+              isWinner: true,
+              isUserWinnder: this.refs['block' + data[0]].className === 'x',
+              playerWin:
+                this.refs['block' + data[0]].className === 'x'
+                  ? parseInt(this.state.playerWin) + 1
+                  : this.state.playerWin,
+              computerWin:
+                this.refs['block' + data[0]].className === 'o'
+                  ? parseInt(this.state.computerWin) + 1
+                  : this.state.computerWin,
+            },
+            () => {
+              this.refs['block' + data[0]].classList.add('blink');
+              this.refs['block' + data[1]].classList.add('blink');
+              this.refs['block' + data[2]].classList.add('blink');
+
+              setTimeout(() => {
+                this.resetGame();
+              }, 2000);
+            }
+          );
+        }
+      }
+    });
+  };
 
   render() {
     return (
@@ -60,15 +201,20 @@ class App extends React.Component {
           <p class='player1'>
             <span class='p1'>You</span>
             <span class='p2'>Player 1</span> (<span class='x'></span>)
-            <span class='score'>{}</span>
+            <span class='score'>{this.state.playerWin}</span>
           </p>
           <p class='ties'>
-            Tie<span class='score'>{}</span>
+            Tie<span class='score'>{this.state.tieGames}</span>
           </p>
           <p class='player2'>
             <span class='p1'>Computer</span>
             <span class='p2'>Player 2</span> (<span class='o'></span>)
-            <span class='score'>{}</span>
+            <span class='score'>{this.state.computerWin}</span>
+          </p>
+          <p className='reset_btn'>
+            <button className='btn btn-primary' onClick={this.resetGame}>
+              RESET
+            </button>
           </p>
         </div>
       </div>
